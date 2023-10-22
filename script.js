ed402fb6a51b1c4663563b8fca4d36041727bb50
AnjaliBharwani <acksacks2405@gmail.com>
2023-10-23 00:43:37 +0530

    ecxcalidraw

---
+++ script.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const c = canvas.getContext("2d");
+
+let drawingColor = "black";
+let previousPosition = null ;
+
+function onMouseDown(e) {
+    previousPosition = [ e.clientX , e.clientY];
+    c.strokeStyle = drawingColor;
+    c.lineWidth = 2; 
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mouseup", onMouseUp); 
+}
+
+function onMouseMove(e){ 
+    // for the first time inside this  
+    let currentPosition = [ e.clientX , e.clientY ];
+    // draw line from previous position to current position ;
+    c.beginPath();
+    c.moveTo(...previousPosition);
+    c.lineTo(...currentPosition);
+    c.stroke();
+    c.closePath();
+    previousPosition = currentPosition ;
+}
+
+function onMouseUp(e){ 
+    canvas.removeEventListener("mousemove", onMouseMove);
+}   
+
